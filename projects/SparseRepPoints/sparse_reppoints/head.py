58f5de57df91518af696221c7fb6e1e10c4e0baa
shenmishajing <shenmishajing@Gmail.com>
2021-03-01 16:28:19 +0800

    implement normalized points and confirm xy shape

--- projects/SparseRepPoints/sparse_reppoints/head.py
+++ projects/SparseRepPoints/sparse_reppoints/head.py
@@ -24,7 +24,6 @@ from .position_encoding import build_position_encoding
 from detectron2.modeling.poolers import ROIPooler, cat
 from detectron2.structures import Boxes
 
-
 _DEFAULT_SCALE_CLAMP = math.log(100000.0 / 16)
 
 
@@ -44,8 +43,8 @@ class SparseRepPointsHead(nn.Module):
         else:
             point_feats, position_feats, center_objectness = self.point_feat_head(features)
 
-        point_feats = torch.cat(point_feats, dim=2)
-        position_feats = torch.cat(position_feats, dim=2)
+        point_feats = torch.cat(point_feats, dim = 2)
+        position_feats = torch.cat(position_feats, dim = 2)
 
         class_logits, pred_bboxes = self.predict_head(point_feats, position_feats)
         return class_logits[None], pred_bboxes[None], center_objectness
@@ -59,7 +58,7 @@ class ObjectnessHead(nn.Module):
         self.channel = channel
         self.k = k
 
-        self.conv = nn.Conv2d(self.channel, 1, 3, padding=1)
+        self.conv = nn.Conv2d(self.channel, 1, 3, padding = 1)
         self.norm = nn.BatchNorm2d(1)
         self.sigmoid = nn.Sigmoid()
 
@@ -100,7 +99,7 @@ class PointFeatHead(nn.Module):
         pts_out_dim = 2 * self.num_points
 
         # Offset Learning.
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace = True)
         self.offset_conv = nn.Conv2d(d_feat, d_feat, 3, 1, 1)
         self.offset_out = nn.Conv2d(d_feat, pts_out_dim, 1, 1, 0)
 
@@ -129,7 +128,7 @@ class PointFeatHead(nn.Module):
         center_objectness = list()
         for index, in_feature in enumerate(self.in_features):
             p = self.objectness_heads[in_feature](features[index])  # [batch_size, 1, w, h]
-            center_objectness.append(p.squeeze(dim=1))
+            center_objectness.append(p.squeeze(dim = 1))
             # xy -> [batch_size, top_k, 2], indice -> [batch_size, top_k]
             xy, indice = self.objectness_heads[in_feature].get_xy(p)
             topk_xys.append(xy)
@@ -139,16 +138,14 @@ class PointFeatHead(nn.Module):
         point_feats = list()
         position_feats = list()
         for i, x in enumerate(features):
-            offset = self.offset_out(self.relu(self.offset_conv(x)))  # [b, 2*num_points, w, h]
+            offset = F.sigmoid(self.offset_out(self.relu(self.offset_conv(x))))  # [b, 2*num_points, w, h]
 
-            topk_idx_repeat = topk_indices[i].unsqueeze(1).repeat([1, 2 * self.num_points, 1])  # [b, 2*num_points, top_k]
-            topk_offset = torch.gather(offset.view((batch_size, 2 * self.num_points, -1)), -1, topk_idx_repeat)
-            topk_points = topk_offset.permute(0, 2, 1) + topk_xys[i].repeat((1, 1, self.num_points))
+            topk_idx_repeat = topk_indices[i][:, :, None].repeat([1, 1, 2 * self.num_points])  # [b, top_k, 2*num_points]
+            topk_offset = torch.gather(offset.reshape((batch_size, 2 * self.num_points, -1)).permute(0, 2, 1), 1, topk_idx_repeat)
+            topk_points = topk_offset.reshape((batch_size, -1, self.num_points, 2)) + topk_xys[i][:, :, None, :].repeat(
+                (1, 1, self.num_points, 1))  # [b, top_k, num_points, 2]
 
-            # todo 归一化到[-1, 1], xy顺序
-            topk_points_normalized = F.tanh(topk_points)  # [b, top_k, 18]
-            topk_points_transposed = topk_points_normalized.view((-1, self.top_k, self.num_points, 2))
-            topk_feat = F.grid_sample(x, topk_points_transposed)  # [b,C,top_k,num_points]，padding
+            topk_feat = F.grid_sample(x, topk_points, padding_mode = 'border')  # [b, C, top_k, num_points], padding
             point_feats.append(topk_feat)
             position_feats.append(self.position_encoding(topk_feat))
 
@@ -170,9 +167,9 @@ class PredictHead(nn.Module):
         d_combine = num_points * (d_feat + d_feat)
         self.combine_fcs = nn.ModuleList([
             nn.Linear(d_combine, d_feat, False),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace = True),
             nn.Linear(d_feat, d_feat, False),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace = True)
         ])
 
         # Use Focal.
@@ -187,7 +184,7 @@ class PredictHead(nn.Module):
         point_feature = point_feature.permute(0, 2, 3, 1).reshape(b, num_object, d)
         position_feature = position_feature.permute(0, 2, 3, 1).reshape(b, num_object, d)
 
-        x = torch.cat([point_feature, position_feature], dim=-1)
+        x = torch.cat([point_feature, position_feature], dim = -1)
         for layer in self.combine_fcs:
             x = layer(x)
         outputs_cls = self.class_embed(x)
